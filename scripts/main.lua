be4f27a48d59412fef8f812581734de23cc5dd07
Rodrigo Delduca <rodrigo@delduca.org>
2024-12-28 14:43:55 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -115,7 +115,7 @@ function setup()
     if value <= 0 then
       octopus.action:set("dead")
 
-      io:rpc("score.incr")
+      io:rpc("octopus.death.incr")
 
       if not timer then
         timemanager:singleshot(3000, function()
