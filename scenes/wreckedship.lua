5ae4063d5b9b03ccca18fdf8f9b0cbb77d55543d
Rodrigo Delduca <rodrigo@delduca.org>
2025-10-20 11:38:31 -0300

    Work in progress

--- scenes/wreckedship.lua
+++ scenes/wreckedship.lua
@@ -1,225 +1,220 @@
 local scene = {}
 
-local pool = {}
-
-local fontfactory = engine:fontfactory()
--- local io = Socket.new()
-local objectmanager = engine:objectmanager()
-local overlay = engine:overlay()
-local postalservice = PostalService.new()
-local scenemanager = engine:scenemanager()
-local statemanager = engine:statemanager()
-local timermanager = engine:timermanager()
-
-local bullet_pool = {}
-local explosion_pool = {}
-local jet_pool = {}
-local segment_pool = {}
-local keystate = {}
+local pool, bullets, bullet_pool, explosion_pool, jet_pool, segment_pool, keystate = {}, {}, {}, {}, {}, {}, {}
 local timer = false
+
+local OFF_BULLET   = { x = -128,  y = -128  }
+local OFF_JET      = { x = 3000,  y = 3000  }
+local JET_OFF_X    = -300
+local PLAYER_Y     = 834
+local FIRE_Y_BASE  = 740
+local FIRE_Y_STEP  = 30
+local rand         = math.random
+
+local active_bullets, active_jets = {}, {}
+
+local function remove_from(list, item)
+  for i = #list, 1, -1 do
+    if list[i] == item then
+      table.remove(list, i)
+      return true
+    end
+  end
+end
+
+local function push_unique(list, item)
+  for i = 1, #list do
+    if list[i] == item then return end
+  end
+  list[#list + 1] = item
+end
+
+local function deactivate(obj, off)
+  obj.action = nil
+  obj.placement = off
+  if obj.velocity and obj.velocity.x then obj.velocity.x = 0 end
+end
 
 local behaviors = {
   hit = function(self)
     if #explosion_pool > 0 then
       local explosion = table.remove(explosion_pool)
-      local offset_x = (math.random(-2, 2)) * 30
-      local offset_y = (math.random(-2, 2)) * 30
+      local offset_x = rand(-2, 2) * 30
+      local offset_y = rand(-2, 2) * 30
       explosion.placement = { x = pool.octopus.x + offset_x, y = pool.player.y + offset_y - 200 }
       explosion.action = "default"
-      timermanager:singleshot(math.random(100, 400), function()
-        if #jet_pool > 0 then
-          local jet = table.remove(jet_pool)
-          local x = 980
-          local base = 812
-          local range = 100
-          local step = 20
-          local y = base + step * math.random(math.floor(-range / step), math.floor(range / step))
-          jet.action = "default"
-          jet.placement = { x, y }
-          jet.velocity = { x = -200 * math.random(3, 6) }
-        end
+
+      timermanager:singleshot(rand(100, 400), function()
+        if #jet_pool == 0 then return end
+        local jet = table.remove(jet_pool)
+        local x = 980
+        local base = 812
+        local range = 100
+        local step = 20
+        local y = base + step * rand(math.floor(-range / step), math.floor(range / step))
+        jet.action = "default"
+        jet.placement = { x, y }
+        jet.velocity = { x = -200 * rand(3, 6) }
+        push_unique(active_jets, jet)
       end)
     end
+
     self.action = "attack"
     self.life = self.life.value - 1
   end,
 }
 
 function scene.on_enter()
-  pool.online = overlay:create(WidgetType.label)
-  pool.online.font = fontfactory:get("fixedsys")
-  pool.online:set(1600, 15)
-
   pool.octopus = scene:get("octopus", SceneType.object)
   pool.octopus.life = 14
   pool.octopus.placement = { x = 1200, y = 732 }
   pool.octopus.action = "idle"
   pool.octopus:on_mail(function(self, message)
     local behavior = behaviors[message]
-    if behavior then
-      behavior(self)
-    end
+    if behavior then behavior(self) end
   end)
+
   pool.octopus.life:subscribe(function(value)
     if next(segment_pool) then
       local segment = table.remove(segment_pool, 1)
       objectmanager:remove(segment)
     end
-    if value <= 0 then
-      pool.octopus.action = "dead"
-      if not timer then
-        timermanager:singleshot(3000, function()
-          scenemanager:set("gameover")
-        end)
-        timer = true
-      end
-    end
+    if value > 0 then return end
+    pool.octopus.action = "dead"
+    if timer then return end
+    timermanager:singleshot(3000, function()
+      scenemanager:set("gameover")
+    end)
+    timer = true
   end)
 
   pool.player = scene:get("player", SceneType.object)
   pool.player.action = "idle"
-  pool.player.placement = { x = 30, y = 834 }
+  pool.player.placement = { x = 30, y = PLAYER_Y }
 
   local segment_matrix = scene:get("segment", SceneType.object)
   for i = 1, 14 do
     local segment = objectmanager:clone(segment_matrix)
     segment.action = "default"
     segment.placement = { x = 1802, y = (i * 14) + 222 }
-    table.insert(segment_pool, segment)
+    segment_pool[#segment_pool + 1] = segment
   end
 
   local bullet_matrix = scene:get("bullet", SceneType.object)
   for i = 1, 3 do
-    local bullet = objectmanager:clone(bullet_matrix)
-    bullet.placement = { x = -128, y = -128 }
+    local b = objectmanager:clone(bullet_matrix)
+    b.placement = OFF_BULLET
 
-    bullet:on_update(function(self)
-      if self.x > pool.octopus.x + 256 then
-        self.action = nil
-        self.placement = { x = -128, y = -128 }
-        self.velocity.x = 0
-        self.action = {}
-        local inpool = false
-        for j = 1, #bullet_pool do
-          if bullet_pool[j] == self then
-            inpool = true
-            break
-          end
-        end
-        if not inpool then
-          table.insert(bullet_pool, self)
-        end
-      end
-    end)
-
-    bullet:on_collision("octopus", function(self, other)
-      self.placement = { x = -128, y = -128 }
+    b:on_collision("octopus", function(self)
+      deactivate(self, OFF_BULLET)
       postalservice:post(Mail.new(pool.octopus, self, "hit"))
-
-      local inpool = false
-      for j = 1, #bullet_pool do
-        if bullet_pool[j] == self then
-          inpool = true
-          break
-        end
-      end
-      if not inpool then
-        table.insert(bullet_pool, self)
-      end
+      remove_from(active_bullets, self)
+      push_unique(bullet_pool, self)
     end)
-    table.insert(bullet_pool, bullet)
+
+    bullet_pool[#bullet_pool + 1] = b
+    bullets[#bullets + 1] = b
   end
 
   for i = 1, 9 do
     local explosion = scene:get("explosion", SceneType.object)
-    explosion.placement = { x = -128, y = -128 }
+    explosion.placement = OFF_BULLET
     explosion:on_end(function(self)
-      self.action = nil
-      self.placement = { x = -128, y = -128 }
-      table.insert(explosion_pool, self)
+      deactivate(self, OFF_BULLET)
+      explosion_pool[#explosion_pool + 1] = self
     end)
-    table.insert(explosion_pool, explosion)
+    explosion_pool[#explosion_pool + 1] = explosion
   end
 
   for i = 1, 9 do
     local jet = scene:get("jet", SceneType.object)
-    jet.placement = { x = 3000, y = 3000 }
+    jet.placement = OFF_JET
     jet:on_collision("player", function(self)
-      self.action = nil
-      self.placement = { x = 3000, y = 3000 }
-      table.insert(jet_pool, self)
+      deactivate(self, OFF_JET)
+      remove_from(active_jets, self)
+      jet_pool[#jet_pool + 1] = self
     end)
-    jet:on_update(function(self)
-      if self.x <= -300 then
-        self.action = nil
-        self.placement = { x = 3000, y = 3000 }
-        table.insert(jet_pool, self)
-      end
-    end)
-    table.insert(jet_pool, jet)
+    jet_pool[#jet_pool + 1] = jet
   end
-
-  -- io:on("online", function(data)
-  -- 	pool.online:set("Online " .. data.clients)
-  -- end)
-  -- io:connect()
 end
 
 function scene.on_loop()
   if statemanager:player(Player.one):on(Controller.left) then
     pool.player.reflection = Reflection.horizontal
     pool.player.velocity = { x = -360 }
-  elseif statemanager:player(Player.one):on(Controller.right) then
+  end
+  if statemanager:player(Player.one):on(Controller.right) then
     pool.player.reflection = Reflection.none
     pool.player.velocity = { x = 360 }
-  else
+  end
+  if not (statemanager:player(Player.one):on(Controller.left) or statemanager:player(Player.one):on(Controller.right)) then
     pool.player.velocity = { x = 0 }
   end
 
-  local action = "run"
-  if pool.player.velocity.x == 0 then
-    action = "idle"
-  end
-  pool.player.action = action
+  pool.player.action = (pool.player.velocity.x == 0) and "idle" or "run"
 
   local pressed = statemanager:player(Player.one):on(Controller.south)
-
   if pressed and not keystate[Controller.south] then
     keystate[Controller.south] = true
-
-    if pool.octopus.life.value <= 0 then
-      return
-    end
-    if #bullet_pool == 0 then
-      return
-    end
+    if pool.octopus.life.value <= 0 then return end
+    if #bullet_pool == 0 then return end
 
     local bullet = table.remove(bullet_pool)
     local x = pool.player.x + 100
-    local y = 740 + math.random(-2, 2) * 30
-
+    local y = FIRE_Y_BASE + rand(-2, 2) * FIRE_Y_STEP
     bullet.placement = { x = x, y = y }
-    bullet.action = nil
     bullet.action = "default"
     bullet.velocity = { x = 800 }
+    push_unique(active_bullets, bullet)
 
-    local sound = "bomb" .. math.random(1, 2)
-    local bomb = scene:get(sound, SceneType.effect)
-    bomb:play()
+    local sound = "bomb" .. rand(1, 2)
+    local sfx = scene:get(sound, SceneType.effect)
+    sfx:play()
+  end
+  if not pressed then keystate[Controller.south] = false end
+
+  for i = #active_bullets, 1, -1 do
+    local b = active_bullets[i]
+    if b.x > pool.octopus.x + 256 then
+      deactivate(b, OFF_BULLET)
+      table.remove(active_bullets, i)
+      push_unique(bullet_pool, b)
+    end
   end
 
-  if not pressed then
-    keystate[Controller.south] = false
+  for i = #active_jets, 1, -1 do
+    local j = active_jets[i]
+    if j.x <= JET_OFF_X then
+      deactivate(j, OFF_JET)
+      table.remove(active_jets, i)
+      jet_pool[#jet_pool + 1] = j
+    end
   end
 end
 
 function scene.on_leave()
-  for name in next, pool do
-    pool[name] = nil
+  timer = nil
+
+  local function clear(t)
+    for i = #t, 1, -1 do t[i] = nil end
+  end
+
+  clear(active_bullets)
+  clear(active_jets)
+  clear(bullets)
+  clear(bullet_pool)
+  clear(explosion_pool)
+  clear(jet_pool)
+  clear(segment_pool)
+
+  for key in next, keystate do
+    keystate[key] = nil
+  end
+  for key in next, pool do
+    pool[key] = nil
   end
 end
 
 sentinel(scene, "wreckedship")
-
 
 return scene
